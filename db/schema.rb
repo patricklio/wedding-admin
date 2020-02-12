c163071964208a044085c15e35d82d633030b60f
nina-tchalla <nina@mobility-sn.com>
2020-02-12 14:24:16 +0000

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_204754) do
+ActiveRecord::Schema.define(version: 2020_02_12_142329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -300,6 +300,16 @@ ActiveRecord::Schema.define(version: 2020_02_09_204754) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_account_id"], name: "index_user_sessions_on_user_account_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "phone_number"
+    t.string "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "vehicle_categories", force: :cascade do |t|
