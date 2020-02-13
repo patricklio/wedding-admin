490a0af8040a2affa07c0bb19307f082b2a85bce
Abdallah Diop <abdallah.diop@mobility-sn.com>
2020-02-13 19:35:09 +0000

    Run Devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_161300) do
+ActiveRecord::Schema.define(version: 2020_02_13_190610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,9 +140,9 @@ ActiveRecord::Schema.define(version: 2020_02_12_161300) do
 
   create_table "joboperations", force: :cascade do |t|
     t.bigint "operation_id", null: false
+    t.bigint "repairoption_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "repairoption_id", null: false
     t.index ["operation_id"], name: "index_joboperations_on_operation_id"
     t.index ["repairoption_id"], name: "index_joboperations_on_repairoption_id"
   end
@@ -315,6 +315,13 @@ ActiveRecord::Schema.define(version: 2020_02_12_161300) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_user_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_user_accounts_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_user_accounts_on_user_id"
   end
 
