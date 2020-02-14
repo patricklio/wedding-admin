e451c517e4515172d9ab9dce1f39a449b22d60f1
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-14 12:10:08 +0000

    add form sample

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,4 +1,9 @@
 class ComponentsController < ApplicationController
   def index
+    @make = Make.all
+    @model = Model.all
+    @fuelType = FuelType.all
+    @vehicleType = VehicleType.all
+    @categories = RepairoptionCategory.all
   end
 end
