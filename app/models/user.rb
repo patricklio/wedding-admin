19878018574e84edce6f5ea54f267aeb7e44689f
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-17 15:39:45 +0000

    add name function to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  #User validation
+  validates :firstname, presence: true, length: { minimum: 2 }
+  validates :lastname, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true, length: { minimum: 9 }
+
+
+  def name
+    " #{ firstname } #{ lastname } "
+  end
 end
