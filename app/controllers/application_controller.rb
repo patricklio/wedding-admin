1cbf9714679a49ec25bddd1bfd6d747c6af61efb
Abdallah Diop <abdallah.diop@mobility-sn.com>
2020-02-17 10:26:34 +0000

    redirect to login path after logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    private
+
+    # logout redirect path
+    def after_sign_out_path_for(resource_or_scope)
+        new_admin_user_account_session_path
+    end
 end
--- app/controllers/user_accounts/sessions_controller.rb
+++ app/controllers/user_accounts/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserAccounts::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   def new
@@ -20,8 +20,7 @@ class UserAccounts::SessionsController < Devise::SessionsController
 
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:admin_user_account, keys: [:email, :password])
+  end
 end
