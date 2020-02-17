0aadcf5117b7f7c4ada0955b7bbd681a58f2b320
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-17 08:52:47 +0000

    move form variable from  component_controller t admin::component_controller

--- app/controllers/admin/components_controller.rb
+++ app/controllers/admin/components_controller.rb
@@ -1,4 +1,11 @@
 class Admin::ComponentsController < ApplicationController
   def index
+    @make = Make.all
+    puts ' ----make----'
+    puts @make
+    @model = Model.all
+    @fuelType = FuelType.all
+    @vehicleType = VehicleType.all
+    @categories = RepairoptionCategory.all
   end
 end
--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,9 +1,4 @@
 class ComponentsController < ApplicationController
   def index
-    @make = Make.all
-    @model = Model.all
-    @fuelType = FuelType.all
-    @vehicleType = VehicleType.all
-    @categories = RepairoptionCategory.all
   end
 end
