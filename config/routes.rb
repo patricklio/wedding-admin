fbf317575bc6d191d725a199700aa2bee9e542d9
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-17 15:41:53 +0000

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,16 @@ Rails.application.routes.draw do
   namespace :admin do
     get "components", to: "components#index"
 
+    as :user_accounts do
+      get 'user_accounts/edit', to:'users#edit', as: :edit_user_profile
+      put 'user_accounts', to:'users#update', as: :update_user_profile
+    end
+
     devise_for :user_accounts,
                skip: [:registrations],
                controllers: {
-                   sessions: 'user_accounts/sessions'
+                   sessions: 'user_accounts/sessions',
+                   passwords: 'user_accounts/passwords'
                },
                path_names: {
                    sign_in: 'login',
