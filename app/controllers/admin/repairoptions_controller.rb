c852df40c2dea7763afd8490ac8727b744e96395
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-18 11:52:38 +0000

    ajout d'une repairoption

--- app/controllers/admin/repairoptions_controller.rb
+++ app/controllers/admin/repairoptions_controller.rb
@@ -1,24 +1,53 @@
 class Admin::RepairoptionsController < ApplicationController
+  before_action :set_repairoption, only: [:edit, :update]
+  before_action :set_new_repairoption, only: [:new]
+
+  def index
+    @repairoptions = Repairoption.all.order('updated_at DESC')
+  end
+
+
   def new
-    @repairoption = Repairoption.new
   end
 
   def create
+    @repairoption = Repairoption.new(ro_params)
+
+    if @repairoption.save
+      if params[:commit] == "Enregistrer"
+        redirect_to admin_repairoptions_path, flash: { success: "Les données ont bien été enregistrées." }
+      else
+        redirect_to edit_admin_repairoption_path(@repairoption), flash: { success: "Les données ont bien été enregistrées." }
+      end
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def edit
-    @repairoption = Repairoption.new
   end
 
   def destroy
   end
 
-  def index
-  end
+
 
   def show
+  end
+
+  private
+  def set_new_repairoption
+    @repairoption = Repairoption.new
+  end
+
+  def set_repairoption
+    @repairoption = Repairoption.find(params[:id])
+  end
+
+  def ro_params
+    params.require(:repairoption).permit(:name, :description, :repairoption_category_id, :optional)
   end
 end
--- app/models/repairoption.rb
+++ app/models/repairoption.rb
@@ -1,2 +1,14 @@
 class Repairoption < ApplicationRecord
+  belongs_to :repairoption_category
+  validates :repairoption_category_id, presence: { message: "La catégorie est obligatoire" }
+  validates :description, presence: {message: "La description du service est obligatoire"}
+  validates :name,
+            presence: {
+                message: "Le nom du service est obligatoire"
+            },
+            uniqueness: {
+                message: ->(object, data) do
+                  "Le service #{data[:value]} existe déja!"
+                end
+            }
 end
