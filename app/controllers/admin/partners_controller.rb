ddfd3b4f87781d4d51be182f4d9b913bab2e43c4
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-18 19:29:41 +0000

    add create and update partner, wwith faorm validation

--- app/controllers/admin/partners_controller.rb
+++ app/controllers/admin/partners_controller.rb
@@ -1,20 +1,52 @@
 class Admin::PartnersController < ApplicationController
+  before_action :set_edit_partner, only: [:destroy, :edit, :update]
+
   def index
     @partners = Partner.all
   end
 
   def new
+    set_new_partner
   end
 
   def create
+    @partner = Partner.new(partner_params)
+
+    if @partner.save
+      redirect_to admin_partners_path, flash: { success: "Les données ont bien été enregistrées." }
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @partner.update(partner_params)
+      redirect_to admin_partners_path, flash: { success: "Les données ont bien été mises à jour." }
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @partner.destroy
+
+    redirect_to admin_partners_path
+  end
+
+  private
+
+  def set_edit_partner
+    @partner = Partner.find(params[:id])
+  end
+
+  def set_new_partner
+    @partner = Partner.new
+  end
+
+  def partner_params
+    params.require(:partner).permit(:name, :address, :phone_number, :email, :labor_hour_price, :labor_currency)
   end
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,60 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "Cette addresse n'est pas valide")
+    end
+  end
+end
+
 class Partner < ApplicationRecord
+  has_many :partner_user_accounts
+  validates :name,
+            presence: {
+                message: "Le nom du partenaire est obligatoire"
+            },
+            length: {
+                minimum: 2,
+                message: "le nom du partenaire est trop court"
+            }
+  validates :address,
+            presence: {
+                message: "L'addresse du partenaire  est obligatoire"
+            },
+            length: {
+                minimum: 2,
+                message: "L'addresse du partenaire est trop courte"
+            }
+  validates :email,
+            presence: {
+                message: "L'email est obligatoire"
+            },
+            uniqueness: {
+                case_sensitive: false,
+                message: ->(object, data) do
+                  "L'adresse email  #{data[:value]} existe déja!"
+                end
+            },
+            email: true
+  validates :phone_number,
+            presence: {
+                message: "Le numero de téléphone est obligatoire"
+            },
+            uniqueness: {
+                message: ->(object, data) do
+                  "Le numéro   #{data[:value]} existe déja!"
+                end, on: :create
+            }
+  validates :labor_hour_price,
+            presence: {
+                message: "Le coût de la main d'oeure horraire est obligatoire"
+            }
+  validates :labor_currency,
+            presence: {
+                message: "La devise est obligatoire"
+            }
+
+
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
 end
