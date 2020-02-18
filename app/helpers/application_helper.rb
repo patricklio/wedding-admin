5687a278b3f4d08f2bb8fc2b4ac069d49892af36
nina-tchalla <nina@mobility-sn.com>
2020-02-18 01:04:44 +0000

    Notice flashed not displayed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def flash_class(level)
     div_class = ""
     case level
-        when "notice" then div_class = "alert alert-info"
+        # when "notice" then div_class = "alert alert-info"
         when "success" then div_class = "alert alert-success"
         when "error" then div_class = "alert alert-error"
         when "alert" then div_class = "alert alert-error"
