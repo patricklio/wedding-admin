ac316fd9a17b628c7fca56f0893a2f1660c46a6d
nina-tchalla <nina@mobility-sn.com>
2020-02-19 17:48:10 +0000

    Hide delete button when category is linked to repairoptions

--- app/models/repairoption_category.rb
+++ app/models/repairoption_category.rb
@@ -11,4 +11,16 @@ class RepairoptionCategory < ApplicationRecord
     }
 
   validates :description, presence: {message: "La description est obligatoire"}
+
+  def self.include_repairoptions_counts
+    joins(
+     %{
+       LEFT OUTER JOIN (
+         SELECT b.repairoption_category_id, COUNT(*) repairoptions_count
+         FROM   repairoptions b
+         GROUP BY b.repairoption_category_id
+       ) a ON a.repairoption_category_id = repairoption_categories.id
+     }
+    ).select("repairoption_categories.*, COALESCE(a.repairoptions_count, 0) AS repairoptions_count")
+  end
 end
