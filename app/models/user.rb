e0350ee652774bfa064cf8d79918f79053fac76a
Lionnel Patrick <patrick@mobility-sn.com>
2020-02-20 12:43:10 +0000

    fix phone number format regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   ROLES = %w(admin sales client).freeze
+  PHONE_NUMBER_REGEX = /((70|76|77|78|30|33)\-)([0-9]{3}\-)([0-9]{2}\-)([0-9]{2})/
 
   has_one :user_account, dependent: :destroy
 
@@ -32,12 +33,16 @@ class User < ApplicationRecord
             }
   validates :phone_number,
             presence: {
-                message: "Le prénom de la catégorie est obligatoire"
+                message: "Le numéro de téléphone est obligatoire"
             },
             uniqueness: {
                 message: ->(object, data) do
                   "Le numéro   #{data[:value]} existe déja!"
                 end, on: :create
+            }, 
+            format: {
+                with: PHONE_NUMBER_REGEX,
+                message: "le format du numéro de téléphone est invalide!, Veuillez utilisez celui-ci: XX-XXX-XX-XX"
             }
   validates :role,
             presence: {
