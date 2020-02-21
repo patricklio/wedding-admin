3b1c233778db294f52f7ec77d798ef62e06044be
Abdallah Diop <abdallah.diop@mobility-sn.com>
2020-02-21 08:21:04 +0000

    Handle dateTime format

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.i18n.available_locales = [:fr, :en]
 end
